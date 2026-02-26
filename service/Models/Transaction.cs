8d823f081a001558d52c106c51a051b5092cada3
msnackey <m.snackey@gmail.com>
2026-02-26 19:44:04 +0100

    chore: Add Transaction model

---
+++ service/Models/Transaction.cs
@@ -0,0 +1,16 @@
+namespace Stoxolio.Service.Models;
+
+public class Transaction
+{
+    public DateOnly Date { get; init; }
+    public TimeOnly Time { get; init; }
+    public string Product { get; init; } = string.Empty;
+    public string Isin { get; init; } = string.Empty;
+    public string Exchange { get; init; } = string.Empty;
+    public int Shares { get; init; }
+    public decimal Price { get; init; }
+    public decimal Value { get; init; }
+    public decimal Fees { get; init; }
+    public decimal Total { get; init; }
+    public string OrderId { get; init; } = string.Empty;
+}
