076e9cb760cca96037cf716ecb2ab9950df71ca7
msnackey <m.snackey@gmail.com>
2026-05-27 21:03:00 +0200

    chore: Move custom CSS

--- client/src/lib/mantine/theme.tsx
+++ client/src/lib/mantine/theme.tsx
@@ -1,5 +1,6 @@
 import { createTheme, MantineProvider } from '@mantine/core'
 import type { PropsWithChildren } from 'react'
+import "./customStyles.css"
 
 const theme = createTheme({
   fontFamily: 'Open Sans, sans-serif',
